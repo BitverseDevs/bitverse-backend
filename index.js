7f302e0f082e8b064bdf7817b9e46bcc1b7c31e0
Mattttyyyy <wehttamocsalb@gmail.com>
2023-04-03 15:33:12 +0800

    fix: revise for test email

--- index.js
+++ index.js
@@ -13,26 +13,38 @@ app.get('/api', function(req, res) {
   res.json(data);
 });
 
-app.post('/send-email', (req, res) => {
+app.post('/send-email', async (req, res) => {
   // Create a transporter object
-  const transporter = nodemailer.createTransport({
-    service: 'gmail',
+  // const transporter = nodemailer.createTransport({
+  //   service: 'gmail',
+  //   auth: {
+  //     user: 'bitverse001@gmail.com',
+  //     pass: 'bitverse123456'
+  //   }
+  // });
+  let testAccount = await nodemailer.createTestAccount();
+
+  let transporter = nodemailer.createTransport({
+    host: "smtp.ethereal.email",
+    port: 587,
+    secure: false, // true for 465, false for other ports
     auth: {
-      user: 'bitverse001@gmail.com',
-      pass: 'bitverse123456'
-    }
+      user: testAccount.user, // generated ethereal user
+      pass: testAccount.pass, // generated ethereal password
+    },
   });
+
 
   // Define your email options
   const mailOptions = {
-    from: 'youremail@gmail.com',
+    from: 'bitverse001@gmail.com',
     to: req.body.to, // Get the recipient email from the request body
     subject: req.body.subject, // Get the email subject from the request body
     text: req.body.message // Get the email message from the request body
   };
 
   // Send the email
-  transporter.sendMail(mailOptions, (error, info) => {
+  await transporter.sendMail(mailOptions, (error, info) => {
     if (error) {
       console.log(error);
       res.status(500).send('Error sending email');
