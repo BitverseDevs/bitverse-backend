fb10218d40df4781405413afaf79e2f4e1b7d76b
Mattttyyyy <wehttamocsalb@gmail.com>
2023-04-03 14:47:38 +0800

    feat: middleware setup body parser for JSONfiles

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const nodemailer = require('nodemailer');
+const bodyParser = require('body-parser');
 const app = express();
 
-
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.get('/api', function(req, res) {
   const data = {
