52dea6edf5b804fbbda0f8b14bcdbc0f17ce487c
Mattttyyyy <wehttamocsalb@gmail.com>
2023-04-03 13:50:31 +0800

    fix: port error heroku

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 
-const port = 8888;
+const port = process.env.PORT || 8888;
 app.listen(port, function() {
   console.log(`Server listening on port ${port}`);
 });
