4f8bb2db55241e341509da6a19d3a4599cc8d522
Mattttyyyy <wehttamocsalb@gmail.com>
2023-04-04 16:49:58 +0800

    feat: check heroku vars

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const Mailgun = require('mailgun.js');
 const mailgun = new Mailgun(formData)
 const DOMAIN = process.env.MAILGUN_DOMAIN;
 const api_key = process.env.MAILGUN_API_KEY;
-const mg = mailgun.client({username: 'api', key: process.env.MAILGUN_API_KEY || api_key});
+const mg = mailgun.client({username: 'api', key: process.env.MAILGUN_API_KEY });
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -148,7 +148,7 @@ app.post('/send-email', (req, res) => {
     )
   .catch(err => {
     res.status(500).send('Error sending email');
-    console.error(err)
+    console.error(err, process.env.MAILGUN_API_KEY, " hyaaa " ,process.env.MAILGUN_DOMAIN )
     }
     );
 });
