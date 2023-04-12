d91e0d935f70529a76261f723f63b05249c2fd22
Mattttyyyy <wehttamocsalb@gmail.com>
2023-04-12 13:38:44 +0800

    chore: delete unused codes

--- index.js
+++ index.js
@@ -12,19 +12,9 @@ const DOMAIN = process.env.MAILGUN_DOMAIN;
 
 const mg = mailgun.client({username: 'api', key: process.env.MAILGUN_API_KEY });
 
-// const allowedOrigins = [
-//     'http://localhost:3000', 
-//     'https://site.bitverseph.com'
-// ];
-
 const allowedOrigins = [ 
     'https://site.bitverseph.com'
 ];
-
-// const corsOption = {
-//     origin: 'http://localhost:3000',
-//     optionsSuccessStatus: 200
-// };
 
 const corsOption = {
     origin: function (origin, callback){
