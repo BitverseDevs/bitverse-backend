d0fa464e59ab1a57c1a27bb703981a411be8edd6
Mattttyyyy <wehttamocsalb@gmail.com>
2023-04-13 09:49:21 +0800

    feat: authService added for authentication

---
+++ services/authService.js
@@ -0,0 +1,63 @@
+// Include required libraries and files here, e.g., bcrypt, jsonwebtoken, User model, Google Authenticator library, etc.
+
+// Define functions for registration, login, and token verification here
+
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const { Authenticator } = require('otplib');
+const User = require('../models/User');
+
+const register = async (email, password) => {
+  const hashedPassword = await bcrypt.hash(password, 10);
+  const twoFactorSecret = generate2FASecret();
+  const newUser = new User({ email, password: hashedPassword, twoFactorSecret });
+  return await newUser.save();
+};
+
+const login = async (email, password, twoFactorToken) => {
+  const user = await User.findOne({ email });
+  if (!user) {
+    throw new Error('User not found');
+  };
+
+  const isPasswordValid = await bcrypt.compare(password, user.password);
+  if (!isPasswordValid) {
+    throw new Error('Invalid password');
+  };
+
+  if (user.twoFactorSecret && twoFactorToken) {
+    const isTokenValid = verify2FAToken(user, twoFactorToken);
+    if (!isTokenValid) {
+      throw new Error('Invalid 2FA token');
+    }
+  };
+
+  const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, { expiresIn: '1h' });
+
+  return { token, user };
+};
+
+const verifyToken = (token) => {
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    return decoded;
+  } catch (error) {
+    return null;
+  }
+};
+
+const generate2FASecret = () => {
+    return Authenticator.generateSecret();
+};
+
+const verify2FAToken = (user, token) => {
+    return Authenticator.verify({ token, secret: user.twoFactorSecret });
+};
+
+module.exports = {
+  register,
+  login,
+  verifyToken,
+  generate2FASecret,
+  verify2FAToken,
+};
