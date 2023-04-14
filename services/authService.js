11f20d2e977d4d7e46440529a5d1f7c11aa5023f
Mattttyyyy <wehttamocsalb@gmail.com>
2023-04-14 09:34:41 +0800

    feat: authService update for recaptcha validation

--- services/authService.js
+++ services/authService.js
@@ -4,8 +4,23 @@
 
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
+const axios = require('axios');
 const { authenticator: Authenticator } = require('otplib');
 const User = require('../models/User');
+
+const verifyRecaptcha = async (recaptchaToken) => {
+  const secretKey = process.env.RECAPTCHA_SECRET_KEY;
+  try {
+    const response = await axios.post(`https://www.google.com/recaptcha/api/siteverify?secret=${secretKey}&response=${recaptchaToken}`);
+    const { success } = response.data;
+
+    if (!success) {
+      throw new Error('Invalid reCAPTCHA token');
+    }
+  } catch (error) {
+    throw new Error('Error verifying reCAPTCHA token');
+  }
+};
 
 const register = async (email, password) => {
   const hashedPassword = await bcrypt.hash(password, 10);
@@ -14,7 +29,10 @@ const register = async (email, password) => {
   return await newUser.save();
 };
 
-const login = async (email, password, twoFactorToken) => {
+const login = async (email, password, twoFactorToken, recaptchaToken) => {
+  if(recaptchaToken){ // TO DO: We can modify this statement to be optionally switchable
+    await verifyRecaptcha(recaptchaToken);
+  }
   const user = await User.findOne({ email });
   if (!user) {
     throw new Error('User not found');
@@ -60,4 +78,5 @@ module.exports = {
   verifyToken,
   generate2FASecret,
   verify2FAToken,
+  verifyRecaptcha,
 };
